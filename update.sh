398c6a9c8dc975ec38ddac350c7505f66c5ac975
Noah Steckley <Noah3141@gmail.com>
2024-10-16 20:26:22 -0400

    "coding used to be hard back in My DaY"

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ echo -e "\nUpdating codebase..."
 git fetch
 git pull
 
-echo -e"\nBuilding the project..."
+echo -e "\nBuilding the project..."
 if npm run build; then
-    echo -e"\nBuild successful. Restarting the service..."
+    echo -e "\nBuild successful. Restarting the service..."
     service rubit restart
 else
-    echo -e"\nBuild failed. Service not restarted."
+    echo -e "\nBuild failed. Service not restarted."
     exit 1
 fi
 
